1d3ba139bdaefbf9fd2768a39cae63279967eca6
Radosław Stolarski <stolarz92@gmail.com>
2015-05-14 10:56:29 +0200

    Exercises from chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+
   def new
     @user = User.new
   end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,7 +4,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "layout links" do
+
+  def setup
+    @user = users(:michael)
+  end
+  test "layout links for non logged in user" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -13,5 +17,18 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "layout links for logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", logout_path
   end
 end
